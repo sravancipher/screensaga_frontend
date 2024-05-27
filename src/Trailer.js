7d48c45ae3b3c9736202c3c0e1d65e1e69d0fcb7
sravancipher <sravanpotnuru24@gmail.com>
2024-05-27 20:53:20 +0530

    updated

--- src/Trailer.js
+++ src/Trailer.js
@@ -4,7 +4,7 @@ function Trailer({name,image}) {
         <>
             <div className='card mb-1 border-0 ratio ratio-21x9' style={{objectFit:"scale-down"}}>
                 <img className="card-img-bottom " src={image} />
-                <div className='card-img-overlay mt-sm-5' style={{ top:"-7px"}}>
+                <div className='card-img-overlay mt-sm-5' style={{ top:"-9px"}}>
                     <p className='card-title text-light' style={{marginTop:"70px"}}>{name}</p>
                     <div className='btn-grid w-100 '>
                         <button className='styledbtn btn text-light' style={{backgroundColor:"transparent",borderColor:"white",border:"2px solid"}}>Watch Now <PlayCircleFilledIcon /></button>
