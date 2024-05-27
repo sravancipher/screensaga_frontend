40fc46273add1db64461efa9cc4a4b24b21597d6
sravancipher <sravanpotnuru24@gmail.com>
2024-05-27 19:19:31 +0530

    updated

--- src/Loginform.js
+++ src/Loginform.js
@@ -15,7 +15,7 @@ function Loginform({changetosignup}){
     const [user_pswd,setPassword]=useState("");
     async function handleSignUp(e){
         e.preventDefault();
-      let url=`http://192.168.1.2:8081/user/login/${user_mail}/${user_pswd}`;
+      let url=`http://https://screensagadb.up.railway.app//user/login/${user_mail}/${user_pswd}`;
         let res=await axios.get(url,{
             
               header:{
