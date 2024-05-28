059cb073a5acacc7ec0820bdf10d0aa77736d3fb
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 20:18:34 +0530

    updated

--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { userobjcontext } from "./Landing";
+import axios from "axios";
 function Contact(){
     const {userobj}=useContext(userobjcontext);
+    const[comment,setComment]=useState("");
+    async function addcomment(e){
+      e.preventDefault();
+      axios.post(`https://screensagadb.up.railway.app/user/addcomment` ,{
+        user_mail: userobj.mail,
+        user_pswd: comment
+     }, {
+        header: {
+           'Content-Type': 'application/json'
+        }
+     })
+     .then(res=>{
+      if(res.data){
+        alert("Your submission has been successfully received")
+      }
+     })
+    }
     return(
         <>
           <div className="container">
           <div className="row">
             <div className="col-md-3 col-1"></div>
             <div className="col-md-6 col-10">
-            <h3 style={{textAlign:"center"}}>Reach Us</h3>
-             <form>
+            <h3 className="bg-danger" style={{textAlign:"center"}}>Reach Us</h3>
+             <form onSubmit={addcomment} method="post">
              <div className="form-floating mb-3 mt-3">
              <input type="email" className="form-control" placeholder="Enter your mail" id="mail"value={userobj.mail} required readOnly/>
              <label className="form-label text-secondary" htmlFor="mail"><b>Email</b></label>
              </div>
              <div className="form-floating mb-3 mt-3">
-              <textarea rows="6" cols="6" className="form-control" placeholder="Write Us" id="write"></textarea>
+              <textarea rows="6" cols="6" className="form-control" placeholder="Write Us" id="write" required value={comment} onChange={()=>{setComment(comment)}}></textarea>
               <label htmlFor="write" className="form-labe">Write to us</label>
              </div>
              <div className="d-grid">
