18b702c1210beefddf6816d8684f720bae41207d
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 09:11:41 +0530

    updated

--- src/Changepswd.js
+++ src/Changepswd.js
@@ -3,11 +3,14 @@ import { useState } from "react";
 function Changepswd({setforgotpassword}){
     const[pswddata,setPswdData]=useState("");
     const [user_mail,setMail]=useState("");
+    const[pswdupdate,checkPswdUpdate]=useState(false);
+    const [user_pswd,setPassword]=useState("");
     async function handleMail(e){
         
         e.preventDefault();
       let url=`https://screensagadb.up.railway.app/user/sendmail/${user_mail}`;
         let res=await axios.put(url,{
+            
               header:{
                  'Content-Type':'application/json'
               }
@@ -17,6 +20,7 @@ function Changepswd({setforgotpassword}){
       //   .catch(err=>{console.log("error"+err)})
         console.log(res.data)
         if(res.data){
+         checkPswdUpdate(true);
          setPswdData("An email notification regarding your password update has been sent to your registered email address")
          setTimeout(() => {
             setforgotpassword();
