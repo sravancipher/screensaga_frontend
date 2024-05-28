55d24871ef5c7c359ae6771bd9a868d5cd388d99
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 22:57:22 +0530

    updated

--- src/Routings.js
+++ src/Routings.js
@@ -144,7 +144,7 @@ function Menubar({watchlistdata,removewatchlist}){
             
             <form >
               <input className="form-control me-2 bg-light " type="text" placeholder="Search" value={searchinput} onChange={(e)=>{searchfn(e)}}/>
-              {input&&<div className="text-light m-2" style={{textAlign:"center",position:"absolute",top:"100px",zIndex:"2"}}>{searchinput}</div>}
+              {input&&<div className="text-light m-2" style={{textAlign:"center",position:"absolute",zIndex:"2",borderColor:"white",border:"2px solid"}}>{searchinput}</div>}
             </form>
           </div>
         </div>
