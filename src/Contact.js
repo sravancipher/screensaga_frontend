8a8c2d06987ef053d5c06e274650167abc3688b5
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 18:31:09 +0530

    updated

--- src/Contact.js
+++ src/Contact.js
@@ -8,14 +8,14 @@ function Contact(){
           <div className="row">
             <div className="col-md-3 col-1"></div>
             <div className="col-md-6 col-10">
-            <h4 style={{textAlign:"center"}}>Reach Us</h4>
+            <h3 style={{textAlign:"center"}}>Reach Us</h3>
              <form>
              <div className="form-floating mb-3 mt-3">
              <input type="email" className="form-control" placeholder="Enter your mail" id="mail"value={userobj.mail} required readOnly/>
              <label className="form-label text-secondary" htmlFor="mail"><b>Email</b></label>
              </div>
              <div className="form-floating mb-3 mt-3">
-              <textarea rows="3" cols="6" className="form-control" placeholder="Write Us" id="write"></textarea>
+              <textarea rows="6" cols="6" className="form-control" placeholder="Write Us" id="write"></textarea>
               <label htmlFor="write" className="form-labe">Write to us</label>
              </div>
              <div className="d-grid">
