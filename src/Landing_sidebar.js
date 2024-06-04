75adcb5dfb9faaebfb001ac7b1e503f796a39469
sravancipher <sravanpotnuru24@gmail.com>
2024-06-04 17:42:52 +0530

    updated

--- src/Landing_sidebar.js
+++ src/Landing_sidebar.js
@@ -1,10 +1,11 @@
 import logo from './images/logo4.png'
 import icon from './images/icon.png'
+import './Landing_sidebar.css'
 function Landing_sidebar(){
     return(
         <>
           <div className="blockquote text-warning" style={{fontFamily:"lucida handwriting"}}>  
-            <h1 className="text-light"style={{fontFamily:"cursive"}} ><img src={icon} style={{width:"7vw"}}/>ScreenSaga</h1>
+            <h1 className="text-light"style={{fontFamily:"cursive"}} ><img className="landinglogo" src={icon}/>ScreenSaga</h1>
             <p >Welcome to Screen Saga,At Screen Saga, you don't just watch movies—you experience them.</p>
             <p> Start your cinematic journey with us today. Your saga begins here.</p>
           </div>
--- src/Watch_the_latest.js
+++ src/Watch_the_latest.js
@@ -2,7 +2,7 @@ import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 function Watch_the_latest({ name, image,ht,t ,playingmovie}) {
     return (
         <>
-                <div className='card mb-4 border-0' >
+                <div className='card mb-4 border-0 ' >
                 <img className="card-img-bottom " src={image} style={{ height:ht,objectFit:"cover" }} />
                 <div className='card-img-overlay mt-sm-5' style={{ top:t }}>
                     <p className='card-title text-light'>{name}</p>
