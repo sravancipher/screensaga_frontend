b88fe34cf3c50978cf1b1290f524607ec92997dc
sravancipher <sravanpotnuru24@gmail.com>
2024-06-04 15:59:46 +0530

    updated

--- src/Landing_sidebar.js
+++ src/Landing_sidebar.js
@@ -1,9 +1,10 @@
 import logo from './images/logo4.png'
+import icon from './images/icon.png'
 function Landing_sidebar(){
     return(
         <>
           <div className="blockquote text-warning" style={{fontFamily:"lucida handwriting"}}>  
-            <h1 className="text-light"style={{fontFamily:"cursive"}} ><img src={logo} style={{width:"10vw"}}/>ScreenSaga</h1>
+            <h1 className="text-light"style={{fontFamily:"cursive"}} ><img src={icon} style={{width:"7vw"}}/>ScreenSaga</h1>
             <p >Welcome to Screen Saga,At Screen Saga, you don't just watch movies—you experience them.</p>
             <p> Start your cinematic journey with us today. Your saga begins here.</p>
           </div>
--- src/Landingpage.js
+++ src/Landingpage.js
@@ -1,6 +1,7 @@
 import Signup from "./Signup"
 import Background_image from "./Background_Image"
 import Landing_sidebar from "./Landing_sidebar"
+import './LandingPage.css'
 
 function Landingpage(){
     
@@ -8,7 +9,7 @@ function Landingpage(){
         <>
         <Background_image/>
             <div className="container">  
-                <div className="row" style={{position:"relative",top:"150px" }}>
+                <div className="row landingdisplay" style={{position:"relative",display:"flex",justifyContent:"center",alignItems:"center"}}>
                     <div className="col-md-6"><Landing_sidebar/></div>
                     <div className="col-md-6"><Signup/></div>
                 </div>
